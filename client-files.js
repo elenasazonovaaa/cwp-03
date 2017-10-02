ddef86552068b3c63700baf17466a3ea6540cdc6
elenasazonovaaa <elenasazonovaaa@gmail.com>
2017-10-02 21:10:59 +0300

    send txt files from client

--- client-files.js
+++ client-files.js
@@ -1,30 +1,58 @@
 const net = require('net');
 const fs = require('fs');
-const port = 8100;
+const path = require('path');
+
+const pathFor = process.argv[2];
+const connect = {host: "127.0.0.1", port: 3001};
+let files = [];
 
 const client = new net.Socket();
 client.setEncoding('utf8');
 
-client.connect(port, function() {
+client.connect(connect, function () {
     console.log('Connected');
+    readFiles();
     client.write('FILES');
 });
-client.on('close', function() {
+client.on('error', (err) => {
+    console.error(err);
+});
+client.on('close', function () {
     console.log('Connection closed');
 });
-client.on('data', function(data,err){
-    if(err) console.log('Err in data client-file');
-    else
-    {
-        if(data === 'ASC')
-        {
-            console.log('Server is asc');
-            client.destroy();
-        }
-        if(data === 'DSC')
-        {
-            onsole.log('Server is dsc');
-            client.destroy();
-        }
+client.on('data', function (data) {
+    if (data === 'ASC' || data === 'NEXT') {
+        sendFiles();
     }
+    if (data === 'DSC') {
+        console.log('Server is dsc');
+        client.destroy();
+    }
+
 });
+
+function sendFiles() {
+    if(files.length !== 0){
+        let file = files.pop();
+        fs.readFile(file,function (err,data) {
+            if(!err) {
+                console.log(data.length);
+                console.log((data+'##'+path.basename(file)+'##'+'END').length);
+                client.write(data+'##'+path.basename(file)+'##'+'END');
+            }
+        });
+    }
+}
+
+function readFiles() {
+    fs.readdir(pathFor, function (err, items) {
+        if (!err) {
+            for (let i = 0; i < items.length; i++) {
+                let filePath = path.join(pathFor, items[i]);
+                fs.stat(filePath, function (err, stat) {
+                    if (!err && stat.isFile()) files.push(filePath);
+                });
+            }
+        }
+    });
+}
--- server.js
+++ server.js
@@ -2,100 +2,113 @@
 const net = require('net');
 const fs = require('fs');
 
-const port = 8100;
-var seed = 0;
+
+const connect = {host: "127.0.0.1", port: 3001};
+let seed = 0;
+let Clients = [];
 
 let questions = [];
 let correct = [];
 let incorrect = [];
+let buffFile = [];
+getJSON();
 
-const server = net.createServer(function(client){
+const server = net.createServer(function (client) {
 
-client.setEncoding('utf8');
+        client.setEncoding('utf8');
+        client.on('error', (err) => {
+            console.error(err);
+        });
+        client.on('data', function (data) {
 
-client.on('data', function(data,err){
-   if(err) console.log(`Error in get data`);
-   else
-   {
-       if(data === 'QA')
-       {
-               client.id = Date.now() + seed++;
-               console.log(' +++ '+'Client-'+ client.id);
-               client.write('ASC');
-               fs.writeFile(client.id+`.txt`,`Client ${client.id} is connect\r\n`,function(err){
-                   if(err) console.log('Err in create LOG');});
-               getJSON();
-       }
-       if(data === 'FILES')
-       {
-           client.id = Date.now() + seed++;
-           console.log(' +++ '+'Client-'+ client.id);
-           client.write('ASC');
-       }
-   }
-});
-client.on('data',function (data,err) {
-    if(err) console.log('Error in get data-2');
-    else
-    {
-        if(data !== 'QA' && data !== 'FILES' )
-        {
-            let flag = false;
-            for(let i = 0; i < questions.length; i++)
-            {
-                if(questions[i] === data)
-                {
-                    if(Date.now()%2 === 0)
-                    {
-                        client.write(correct[i].toString());
-                        fs.appendFile(client.id+`.txt`,
-                            'Questions: '+ questions[i].toString()+ `\r\n` + 'Answer: '+ correct[i].toString()+ `\r\n` ,
-                            (err)=>{if(err) console.log('Err in create LOG');});
+            if (data === 'QA') {
+                isFileClient = false;
+                client.id = Date.now() + seed++;
+                console.log(' +++ ' + 'Client-' + client.id);
+                client.write('ASC');
+                fs.writeFile(client.id + `.txt`, `Client ${client.id} is connect\r\n`, function (err) {
+                    if (err) console.log('Err in create LOG');
+                });
+            }
+            if (data === 'FILES') {
+                client.id = Date.now() + seed++;
+                Clients[client.id] = data;
+                buffFile[client.id] = [];
+                console.log(' +++ ' + 'Client-' + client.id);
+                client.write('ASC');
+            }
+        });
+        client.on('data', function (data) {
+
+            if (data !== 'QA' && data !== 'FILES') {
+                for (let i = 0; i < questions.length; i++) {
+                    if (questions[i] === data) {
+                        if (Date.now() % 2 === 0) {
+                            client.write(correct[i].toString());
+                            fs.appendFile(client.id + `.txt`,
+                                'Questions: ' + questions[i].toString() + `\r\n` + 'Answer: ' + correct[i].toString() + `\r\n`,
+                                (err) => {
+                                    if (err) console.log('Err in create LOG');
+                                });
+                        }
+                        else {
+                            client.write(incorrect[i].toString());
+                            fs.appendFile(client.id + `.txt`,
+                                'Questions: ' + questions[i].toString() + `\r\n` + 'Answer: ' + incorrect[i].toString() + `\r\n`,
+                                (err) => {
+                                    if (err) console.log('Err in create LOG');
+                                });
+                        }
                     }
-                    else
-                    {
-                        client.write(incorrect[i].toString());
-                        fs.appendFile(client.id+`.txt`,
-                            'Questions: '+ questions[i].toString()+ `\r\n` + 'Answer: '+ incorrect[i].toString()+ `\r\n`,
-                            (err)=>{if(err) console.log('Err in create LOG');});
-                    }
-                    flag = true;
                 }
             }
-            if(!flag) client.write('DSC');
-        }
-    }
+        });
+        client.on('data',function (data) {
+            if(Clients[client.id] === 'FILES' && data !== 'FILES'){
+               buffFile[client.id].push(Buffer.from(data));
+               if(data.toString().endsWith('END')) {
+                   createFile(client.id);
+                   client.write('NEXT');
+               }
+            }
+        });
+        client.on('end', function () {
+            if (client.id === undefined) console.log(' --- ' + 'no connect for Client');
+            else {
+                //fs.appendFile(client.id+`.txt`,'Client disconnected :)',(err)=>{if(err) console.log('Err in create LOG');});
+                console.log(' --- ' + 'Client-' + client.id);
+            }
+        });
+    })
+;
+
+server.listen(connect, () => {
+    console.log(`Server listening on localhost:${connect.host}:${connect.port}`);
+    console.log(process.env.pathSave.toString());
 });
 
-
-client.on('end', function(){
-    if(client.id === undefined) console.log(' --- '+'no connect for Client');
-    else
-    {
-        //fs.appendFile(client.id+`.txt`,'Client disconnected :)',(err)=>{if(err) console.log('Err in create LOG');});
-        console.log(' --- '+'Client-' + client.id);
-    }
-    } );
-
-});
-
-server.listen(port, () => {
-    console.log(`Server listening on localhost:${port}`);
-});
-
-function getJSON()
-{
-    fs.readFile('qa.json',function (err,data) {
-        if(err) console.log('Error in read JSON');
-        else
-        {
+function getJSON() {
+    fs.readFile('qa.json', function (err, data) {
+        if (err) console.log('Error in read JSON');
+        else {
             let json = JSON.parse(data);
-            for(let i = 0; i < json.length;i++)
-            {
+            for (let i = 0; i < json.length; i++) {
                 questions[i] = json[i].question;
                 correct[i] = json[i].correct;
                 incorrect[i] = json[i].incorrect;
             }
         }
     })
+}
+function createFile(id) {
+    let fileData = Buffer.concat(buffFile[id]);
+    let fileName = fileData.toString().split('##')[1];
+    let dataInFile = fileData.toString().split('##')[0];
+    console.log(`name ${fileName} -- ${dataInFile.length}`);
+    fs.writeFile(process.env.pathSave + '\\' + fileName, dataInFile , function (err) {
+            if (err)
+                console.error(err);
+        }
+    );
+    buffFile[id]=[];
 }
